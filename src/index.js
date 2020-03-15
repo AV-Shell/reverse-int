f80e94e9e0a4f26a97d1791975eff6d6f501c8c7
AV-Shell <avshell.sj@gmail.com>
2020-03-15 13:33:34 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+
+    console.log (n,(n + ''), (n + '').match(/\d/g),(n + '').match(/\d/g).reverse(),(n + '').match(/\d/g).reverse().join(''),  +((n + '').match(/\d/g).reverse().join('')));
+ return  +((n + '').match(/\d/g).reverse().join(''));
 }
